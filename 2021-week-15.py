13004f92fe263dc6b71770c7e9e98ea18f254535
Will Sutton <sutton_999@hotmail.com>
2021-04-16 10:21:56 +0100

    uploading week 15 solution

--- 2021-week-15.py
+++ 2021-week-15.py
@@ -6,38 +6,6 @@ import numpy as np
 menu = pd.read_excel('unprepped_data\\PD 2021 Wk 15 Input - Menu and Orders.xlsx', engine='openpyxl', sheet_name = 'MENU')
 order = pd.read_excel('unprepped_data\\PD 2021 Wk 15 Input - Menu and Orders.xlsx', engine='openpyxl', sheet_name = 'Order')
 
-
-def explode(df, lst_cols, fill_value='', preserve_index=False):
-    # make sure `lst_cols` is list-alike
-    if (lst_cols is not None
-        and len(lst_cols) > 0
-        and not isinstance(lst_cols, (list, tuple, np.ndarray, pd.Series))):
-        lst_cols = [lst_cols]
-    # all columns except `lst_cols`
-    idx_cols = df.columns.difference(lst_cols)
-    # calculate lengths of lists
-    lens = df[lst_cols[0]].str.len()
-    # preserve original index values    
-    idx = np.repeat(df.index.values, lens)
-    # create "exploded" DF
-    res = (pd.DataFrame({
-                col:np.repeat(df[col].values, lens)
-                for col in idx_cols},
-                index=idx)
-             .assign(**{col:np.concatenate(df.loc[lens>0, col].values)
-                            for col in lst_cols}))
-    # append those rows that have empty lists
-    if (lens == 0).any():
-        # at least one list in cells is empty
-        res = (res.append(df.loc[lens==0, idx_cols], sort=False)
-                  .fillna(fill_value))
-    # revert the original index order
-    res = res.sort_index()
-    # reset index if requested
-    if not preserve_index:        
-        res = res.reset_index(drop=True)
-    return res
-
 # Modify the structure of the Menu table so we can have one column for:
 #  - the Type (pizza, pasta, house plate), 
 #  - the name of the plate, 
@@ -64,48 +32,93 @@ house_plates['Type'] = 'House Plates'
 menu_df = pd.concat([pizza,pasta,house_plates])
 menu_df = menu_df[menu_df.ID.notnull()]
 
+# Modify the structure of the Orders table to have each item ID in a different row 
+
 order['ID'] = order.index
-print(order)
 
-
-#df.col.str.split(expand=True).head()
-##s = order['Order'].str.split('-',expand=True).stack()
-#combined = pd.merge(order,s, how='left', on=index)
-
-#a = explode(order, ['Order'], fill_value='-')
-
+# split string into seperate columns
 a = pd.DataFrame(order['Order'].str.split('-',expand=True), index=order['ID'])
-#a = pd.DataFrame(order['Order'].str.split('-').tolist(), index=order['Customer Name']).stack()
-print(a)
+
 a.columns = ['1','2','3','4']
-print(a.columns)
 a['ID'] = a.index
 
-print(a)
+# seperate into 2 column dataframes
 a1 = a[['ID','1']]
 a2 = a[['ID','2']]
-print(a1)
-#split_list = list(order['Order'].str.split('-'))
-#print(split_list)
-#better_split_list = [x if type(x) != np.float else [None,None] for x in split_list]
-#print(better_split_list)
-#a = explode(order.assign(var1=order['Order'].str.split('-')), 'Order')
-# = pd.DataFrame(order['Order'].str.split('-').tolist(), index=order[['Customer Name', 'Order Date']]).stack()
-#b = b.reset_index()[[0, 'var2']] # var1 variable is currently labeled 0
-#b.columns = ['var1', 'var2'] # renaming var1
+a3 = a[['ID','3']]
+a4 = a[['ID','4']]
 
-#pd.concat([Series(row['var2'], row['var1'].split(','))              
-#                    for _, row in a.iterrows()]).reset_index()
-#print(combined)
-# Modify the structure of the Orders table to have each item ID in a different row 
+ # rename all columns
+a1.columns = ['ID','Order ID']
+a2.columns = a1.columns
+a3.columns = a1.columns
+a4.columns = a1.columns
+
+# stack dataframes and remove null entries
+order_ids = pd.concat([a1,a2,a3,a4])
+order_ids = order_ids[order_ids['Order ID'].notnull()]
+del order_ids['ID']
+
+# join back to original orders table and delete unused columns
+order_df = pd.merge(order,order_ids, on='ID', how = 'left')
+order_df['Order ID'] = np.where(order_df['Order ID'].notnull(), order_df['Order ID'], order_df['Order'])
+del order_df['Order']
+del order_df['ID']
 
 # Join both tables 
 
+# convert join columns to int
+order_df = order_df.astype({'Order ID': int})
+menu_df = menu_df.astype({'ID': int})
+
+# join menu with orders
+takings_df = pd.merge(menu_df, order_df, left_on='ID', right_on='Order ID', how='left')
+
 # On Monday's we offer a 50% discount on all items. Recalculate the prices to reflect this
+# work out weekday and apply discount
+takings_df['weekday'] = takings_df['Order Date'].dt.dayofweek
+# Note Mondays are denoted as 0
+takings_df['Price Paid'] = np.where(takings_df['weekday'] == 0 , takings_df['Price']/2, takings_df['Price'])
+
+# convert weekday to day name
+takings_df['weekday'] = np.select(
+    [
+        takings_df['weekday'] == 0, 
+        takings_df['weekday'] == 1,
+        takings_df['weekday'] == 2, 
+        takings_df['weekday'] == 3,
+        takings_df['weekday'] == 4, 
+        takings_df['weekday'] == 5,
+        takings_df['weekday'] == 6
+    ], 
+    [
+        'Monday', 
+        'Tuesday',
+        'Wednesday', 
+        'Thursday',
+        'Friday', 
+        'Saturday',
+        'Sunday'
+    ], 
+    default='Unknown'
+)
+#print(takings_df)
 
 # For Output 1, we want to calculate the total money for each day of the week 
+output_1 = takings_df[['weekday','Price Paid']]
+output_1 = output_1.groupby(['weekday']).sum()
 
 # For Output 2, we want to reward the customer who has made the most orders for their loyalty. 
 # Work out which customer has ordered the most single items.
+output_2 = takings_df[['Customer Name','Order ID']]
+output_2 = output_2.groupby(['Customer Name']).agg('count')
 
+max_orders = output_2['Order ID'].max()
+output_2 = output_2.loc[output_2['Order ID'] == max_orders]
+output_2.columns = ['Count Items']
 
+# Writing data to csv
+output_1.to_csv('prepped_data\\PD 2021 Wk 15 Output - Weekday Sales.csv', index=True)
+output_2.to_csv('prepped_data\\PD 2021 Wk 15 Output - Top Customer.csv', index=True)
+
+print("data prepped!")
