a0ecb97b4b137afd4c4fb65e6899bc527b3097cc
Will Sutton <wjsutton.dev@outlook.com>
2023-07-04 16:44:29 +0100

    update SQL

--- 2023/SQL/2023_week_01.sql
+++ 2023/SQL/2023_week_01.sql
@@ -1,74 +1,38 @@
 -- Preppin' Data 2023 Week 01
-/*
-Split the Transaction Code to extract the letters at the start of the transaction code. 
-These identify the bank who processes the transaction 
-Rename the new field with the Bank code 'Bank'. 
-Rename the values in the Online or In-person field, Online of the 1 values and In-Person for the 2 values. 
-Change the date to be the day of the week
-Different levels of detail are required in the outputs. You will need to sum up the values of the transactions in three ways (help):
-1. Total Values of Transactions by each bank
-2. Total Values by Bank, Day of the Week and Type of Transaction (Online or In-Person)
-3. Total Values by Bank and Customer Code
 
+-- Split the Transaction Code to extract the letters at the start of the transaction code. These identify the bank who processes the transaction 
+    -- Rename the new field with the Bank code 'Bank'. 
+-- Rename the values in the Online or In-person field, Online of the 1 values and In-Person for the 2 values. 
+-- Change the date to be the day of the week
+-- Different levels of detail are required in the outputs. You will need to sum up the values of the transactions in three ways:
+    -- 1. Total Values of Transactions by each bank
+    -- 2. Total Values by Bank, Day of the Week and Type of Transaction (Online or In-Person)
+    -- 3. Total Values by Bank and Customer Code
+
+-- 1. Total Values of Transactions by each bank
 SELECT 
-LEFT(transaction_code,charindex('-', transaction_code)-1) as bank,
-CASE online_or_in_person
-WHEN 1 THEN 'Online'
-WHEN 2 THEN 'In-Person'
-END as online_or_in_person,
-to_char((RIGHT(LEFT(transaction_date,10),4) || '-' || RIGHT(LEFT(transaction_date,5),2) || '-' || LEFT(transaction_date,2))::date, 'dy')  as transaction_date,
+SPLIT_PART(transaction_code,'-',1) as bank,
+SUM(value) as total_value
+FROM pd2023_wk01
+GROUP BY SPLIT_PART(transaction_code,'-',1);
+
+-- 2. Total Values by Bank, Day of the Week and Type of Transaction (Online or In-Person)
+SELECT
+SPLIT_PART(transaction_code,'-',1) as bank,
+CASE 
+WHEN online_or_in_person=1 THEN 'Online'
+WHEN online_or_in_person=2 THEN 'In-Person'
+END as online_in_person,
+DAYNAME(DATE(transaction_date,'dd/MM/yyyy hh24:mi:ss')) as day_of_week,
+SUM(value) as total_value
+FROM pd2023_wk01
+GROUP BY 1,2,3;
+
+-- 3. Total Values by Bank and Customer Code
+SELECT
+SPLIT_PART(transaction_code,'-',1) as bank,
 customer_code,
-value
-FROM PD2023_WK01
-*/
-
--- OUTPUT 1 
-SELECT 
-LEFT(transaction_code,charindex('-', transaction_code)-1) as bank,
-SUM(value) as value
-FROM PD2023_WK01
-GROUP BY LEFT(transaction_code,charindex('-', transaction_code)-1);
-
--- OUTPUT 2 
-WITH CTE AS (
-SELECT 
-LEFT(transaction_code,charindex('-', transaction_code)-1) as bank,
-CASE online_or_in_person
-WHEN 1 THEN 'Online'
-WHEN 2 THEN 'In-Person'
-END as online_or_in_person,
-to_char((RIGHT(LEFT(transaction_date,10),4) || '-' || RIGHT(LEFT(transaction_date,5),2) || '-' || LEFT(transaction_date,2))::date, 'dy') as transaction_date,
-value
-FROM PD2023_WK01
-)
-
-SELECT 
-bank,
-online_or_in_person,
-CASE transaction_date
-    WHEN 'Tue' THEN 'Tuesday'
-    WHEN 'Wed' THEN 'Wednesday'
-    WHEN 'Thu' THEN 'Thursday'
-    WHEN 'Sat' THEN 'Saturday'
-    ELSE transaction_date || 'day'
-END as transaction_date,
-SUM(value) as value
-FROM CTE
-GROUP BY bank,
-online_or_in_person,
-CASE transaction_date
-    WHEN 'Tue' THEN 'Tuesday'
-    WHEN 'Wed' THEN 'Wednesday'
-    WHEN 'Thu' THEN 'Thursday'
-    WHEN 'Sat' THEN 'Saturday'
-    ELSE transaction_date || 'day'
-END;
-
--- OUTPUT 3
-SELECT 
-LEFT(transaction_code,charindex('-', transaction_code)-1) as bank,
-customer_code,
-SUM(value) as value
-FROM PD2023_WK01
-GROUP BY LEFT(transaction_code,charindex('-', transaction_code)-1),
+SUM(value) as total_value
+FROM pd2023_wk01
+GROUP BY SPLIT_PART(transaction_code,'-',1),
 customer_code;
--- 2023/SQL/2023_week_04.sql
+++ 2023/SQL/2023_week_04.sql
@@ -1,78 +1,91 @@
+-- Preppin' Data 2023 Week 04
 
--- We want to stack the tables on top of one another, since they have the same fields in each sheet.
+-- We want to stack the tables on top of one another, since they have the same fields in each sheet. We can do this one of 2 ways:
+    -- Drag each table into the canvas and use a union step to stack them on top of one another
+    -- Use a wildcard union in the input step of one of the tables
 -- Some of the fields aren't matching up as we'd expect, due to differences in spelling. Merge these fields together
 -- Make a Joining Date field based on the Joining Day, Table Names and the year 2023
--- Now we want to reshape our data so we have a field for each demographic, for each new customer
+-- Now we want to reshape our data so we have a field for each demographic, for each new customer (help)
 -- Make sure all the data types are correct for each field
--- Remove duplicates
--- If a customer appears multiple times take their earliest joining date
-WITH DATA_STACK AS (
-    SELECT *,'january' as table_name  FROM pd2023_wk04_january
-        UNION ALL
-    SELECT *,'february' as table_name FROM pd2023_wk04_february
-        UNION ALL
-    SELECT *,'march' as table_name FROM pd2023_wk04_march
-        UNION ALL
-    SELECT *,'april' as table_name FROM pd2023_wk04_april 
-        UNION ALL
-    SELECT *,'may' as table_name FROM pd2023_wk04_may 
-        UNION ALL
-    SELECT *,'june' as table_name FROM pd2023_wk04_june 
-        UNION ALL
-    SELECT *,'july' as table_name FROM pd2023_wk04_july
-        UNION ALL
-    SELECT *,'august' as table_name FROM pd2023_wk04_august
-        UNION ALL
-    SELECT *,'september' as table_name FROM pd2023_wk04_september
-        UNION ALL
-    SELECT *,'october' as table_name FROM pd2023_wk04_october
-        UNION ALL
-    SELECT *,'november' as table_name FROM pd2023_wk04_november
-        UNION ALL
-    SELECT *,'december' as table_name FROM pd2023_wk04_december 
+-- Remove duplicates (help)
+    -- If a customer appears multiple times take their earliest joining date
+WITH CTE AS (
+SELECT *, 'pd2023_wk04_january' as tablename FROM pd2023_wk04_january
+
+UNION ALL 
+
+SELECT *, 'pd2023_wk04_february' as tablename FROM pd2023_wk04_february
+
+UNION ALL 
+
+SELECT *, 'pd2023_wk04_march' as tablename FROM pd2023_wk04_march
+
+UNION ALL 
+
+SELECT *, 'pd2023_wk04_april' as tablename FROM pd2023_wk04_april
+
+UNION ALL
+
+SELECT *, 'pd2023_wk04_may' as tablename FROM pd2023_wk04_may
+
+UNION ALL
+
+SELECT *, 'pd2023_wk04_june' as tablename FROM pd2023_wk04_june
+
+UNION ALL
+
+SELECT *, 'pd2023_wk04_july' as tablename FROM pd2023_wk04_july
+
+UNION ALL
+
+SELECT *, 'pd2023_wk04_august' as tablename FROM pd2023_wk04_august
+
+UNION ALL
+
+SELECT *, 'pd2023_wk04_september' as tablename FROM pd2023_wk04_september
+
+UNION ALL
+
+SELECT *, 'pd2023_wk04_october' as tablename FROM pd2023_wk04_october
+
+UNION ALL
+
+SELECT *, 'pd2023_wk04_november' as tablename FROM pd2023_wk04_november
+
+UNION ALL
+
+SELECT *, 'pd2023_wk04_december' as tablename FROM pd2023_wk04_december
 )
-,PRE_PIVOT AS (
-    SELECT 
-    id,
-    date_from_parts(2023
-    ,CASE UPPER(LEFT(table_name,3))
-           WHEN 'JAN' THEN 1
-           WHEN 'FEB' THEN 2
-           WHEN 'MAR' THEN 3
-           WHEN 'APR' THEN 4
-           WHEN 'MAY' THEN 5
-           WHEN 'JUN' THEN 6
-           WHEN 'JUL' THEN 7
-           WHEN 'AUG' THEN 8
-           WHEN 'SEP' THEN 9
-           WHEN 'OCT' THEN 10
-           WHEN 'NOV' THEN 11
-           WHEN 'DEC' THEN 12
-           ELSE NULL
-    END
-    ,joining_day) as joining_date,
-    demographic,
-    value
-    FROM DATA_STACK
+, PRE_PIVOT AS (
+SELECT 
+id,
+date_from_parts(2023,DATE_PART('month',DATE(SPLIT_PART(tablename,'_',3),'MMMM')),joining_day) as joining_date,
+demographic,
+value
+FROM CTE
 )
 ,POST_PIVOT AS (
-    SELECT 
-    id::int as id,
-    joining_date::date as joining_date,
-    ethnicity,
-    account_type,
-    date_of_birth::date as date_of_birth,
-    ROW_NUMBER() OVER(PARTITION BY id ORDER BY joining_date::date ASC) as rn
-    FROM PRE_PIVOT
-    PIVOT(max(value) for demographic in ('Ethnicity','Account Type','Date of Birth'))
-    AS P (id,joining_date,ethnicity,account_type,date_of_birth)
-    ORDER BY id
+SELECT 
+id,
+joining_date,
+ethnicity,
+account_type,
+date_of_birth::date as date_of_birth,
+ROW_NUMBER() OVER(PARTITION BY id ORDER BY joining_date ASC) as rn
+FROM PRE_PIVOT
+PIVOT(MAX(value) FOR demographic IN ('Ethnicity','Account Type','Date of Birth')) AS P
+(id,
+joining_date,
+ethnicity,
+account_type,
+date_of_birth)
 )
 SELECT 
-    id,
-    joining_date,
-    ethnicity,
-    account_type,
-    date_of_birth
-FROM POST_PIVOT
-WHERE rn = 1;
+id,
+joining_date,
+account_type,
+date_of_birth,
+ethnicity
+FROM post_pivot
+WHERE rn = 1
+
